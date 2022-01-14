3d852a814ac91ba424dbc796eabf88dcd87b25d8
dohyeon-han <limited.hyeon@gmail.com>
2022-01-14 21:40:18 +0900

    close #3 로그인, jwt Junit test

--- src/main/java/com/todayhouse/security/config/JwtTokenProvider.java
+++ src/main/java/com/todayhouse/security/config/JwtTokenProvider.java
@@ -5,7 +5,6 @@ import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -22,8 +21,7 @@ import java.util.List;
 @Component
 public class JwtTokenProvider {
 
-    @Value("spring.jwt.secret")
-    private String secretKey;
+    private String secretKey = "todayzip";
 
     // 토큰 유효시간 30분
     private final long tokenValidTime = 30 * 60 * 1000L;
---
+++ src/test/java/com/todayhouse/security/api/UserControllerTest.java
@@ -0,0 +1,102 @@
+package com.todayhouse.security.api;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.todayhouse.security.config.JwtTokenProvider;
+import com.todayhouse.security.domian.user.User;
+import com.todayhouse.security.domian.user.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+class UserControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    JwtTokenProvider jwtTokenProvider;
+
+    @BeforeEach
+    void clearRepository(){
+        userRepository.deleteAll();
+    }
+
+    @Test
+    void 유저등록() throws Exception{
+        Map<String, String> user = new HashMap<>();
+        user.put("email","test@t.com");
+        user.put("password","12345");
+        String url = "http://localhost:8080/api/join";
+
+        mockMvc.perform(MockMvcRequestBuilders.post(url)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(user)))
+                .andExpect(status().isOk());
+
+        String email = userRepository.findByEmail("test@t.com").get().getEmail();
+        assertEquals(email,user.get("email"));
+    }
+
+    @Test
+    void 로그인() throws Exception{
+        Map<String, String> user = new HashMap<>();
+        user.put("email","test@t.com");
+        user.put("password","12345");
+        String url = "http://localhost:8080/api/login";
+
+        userRepository.save(User.builder()
+                .email("test@t.com")
+                .password(new BCryptPasswordEncoder().encode("12345"))
+                .roles(Collections.singletonList("ROLE_USER"))
+                .build());
+
+        mockMvc.perform(MockMvcRequestBuilders.post(url)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(new ObjectMapper().writeValueAsString(user)))
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
+
+    @Test
+    void jwtTest() throws Exception {
+        userRepository.save(User.builder()
+                .email("test@.com")
+                .password("12345")
+                .roles(Collections.singletonList("ROLE_USER"))
+                .build());
+        String url = "http://localhost:8080/api/test";
+        String jwt = jwtTokenProvider.createToken("test", Collections.singletonList("ROLE_USER"));
+
+        mockMvc.perform(MockMvcRequestBuilders.get(url)
+                        .header("X-AUTH-TOKEN",jwt)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(print());
+
+        mockMvc.perform(MockMvcRequestBuilders.get(url)
+                        .header("X-AUTH-TOKEN",jwt.replace("1","2"))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().is4xxClientError())
+                .andDo(print());
+    }
+
+
+}
