ee74e20e3b4d21df48569e9f248a2d7205d7ac99
dohyeon-han <limited.hyeon@gmail.com>
2022-01-19 21:00:19 +0900

    서브모듈로 jwt secret key 사용

--- src/main/java/com/todayhouse/global/config/WebSecurityConfig.java
+++ src/main/java/com/todayhouse/global/config/WebSecurityConfig.java
@@ -4,7 +4,9 @@ import com.todayhouse.global.config.jwt.JwtAuthenticationFilter;
 import com.todayhouse.global.config.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
@@ -19,6 +21,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
 
+@EnableConfigurationProperties
 @RequiredArgsConstructor
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
