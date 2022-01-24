290cc32c99c061f62a4380d43c7c8f4404841d45
dohyeon-han <limited.hyeon@gmail.com>
2022-01-24 22:10:47 +0900

    close #16 import 최적화

--- src/main/java/com/todayhouse/domain/email/dto/request/EmailSendRequest.java
+++ src/main/java/com/todayhouse/domain/email/dto/request/EmailSendRequest.java
@@ -1,6 +1,9 @@
 package com.todayhouse.domain.email.dto.request;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
