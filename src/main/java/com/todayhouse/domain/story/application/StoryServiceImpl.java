4cff04044bd67695ab21fd2e5f551b846df34255
DOWON LEE <domo7304@naver.com>
2022-03-27 17:33:02 +0900

    amazon s3 url 매칭을 dto에서 service로 이동

--- src/main/java/com/todayhouse/domain/story/application/StoryServiceImpl.java
+++ src/main/java/com/todayhouse/domain/story/application/StoryServiceImpl.java
@@ -1,6 +1,7 @@
 package com.todayhouse.domain.story.application;
 
 import com.todayhouse.domain.image.application.ImageService;
+import com.todayhouse.domain.image.domain.Image;
 import com.todayhouse.domain.story.dao.StoryRepository;
 import com.todayhouse.domain.story.domain.Story;
 import com.todayhouse.domain.story.dto.reqeust.StoryCreateRequest;
@@ -64,7 +65,8 @@ public class StoryServiceImpl implements StoryService {
 
     @Override
     public StoryGetDetailResponse findById(Long id) {
-        return new StoryGetDetailResponse(this.getStory(id));
+
+        return new StoryGetDetailResponse(this.getStory(id), this.getStory(id).getImages().stream().map(image -> fileService.changeFileNameToUrl(image.getFileName())).collect(Collectors.toList()));
     }
 
     private Story getStory(Long id) {
