db1db0d19fb4bfef59308c32edf3e32a2755327e
dohyeon-han <limited.hyeon@gmail.com>
2022-04-29 17:45:12 +0900

    review entity 구현

---
+++ src/main/java/com/todayhouse/domain/review/domain/Review.java
@@ -0,0 +1,48 @@
+package com.todayhouse.domain.review.domain;
+
+import com.todayhouse.domain.product.domain.Product;
+import com.todayhouse.domain.user.domain.User;
+import com.todayhouse.global.common.BaseTimeEntity;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+
+@Getter
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Review extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private int liked = 0;
+
+    private int rating;
+
+    private String content;
+
+    @Column(name = "review_image")
+    private String reviewImage;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @Builder
+    public Review(int rating, String content, String reviewImage, User user, Product product){
+        this.rating = rating;
+        this.content = content;
+        this.reviewImage = reviewImage;
+        this.user = user;
+        this.product = product;
+    }
+}
