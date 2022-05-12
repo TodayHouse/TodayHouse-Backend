041acacaf19294795f74d482028481226e5c4d11
suker <suker800@gmail.com>
2022-05-12 15:13:14 +0900

    Story 생성 시간 필드 추가

--- src/main/java/com/todayhouse/domain/story/domain/Story.java
+++ src/main/java/com/todayhouse/domain/story/domain/Story.java
@@ -7,8 +7,10 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -32,6 +34,10 @@ public class Story extends BaseTimeEntity {
     @Column(nullable = false)
     private String content;
 
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+
 //    기본 게시글 기능 정상 동작 확인 후 상품 클릭 시 링크 연결 기능 추가
 //    @Column(name = "product_link")
 //    private String productLink;
@@ -49,6 +55,7 @@ public class Story extends BaseTimeEntity {
 
     @OneToMany(mappedBy = "story", cascade = CascadeType.ALL, orphanRemoval = true)
     private final List<StoryImage> images = new ArrayList<>();
+
 
     @Builder
     public Story(String title, String content, Integer liked, Category category, User user) {
