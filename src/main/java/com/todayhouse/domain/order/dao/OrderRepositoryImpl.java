dab236e9e85387d545e4e73883babe666585917c
dohyeon-han <limited.hyeon@gmail.com>
2022-05-20 22:20:45 +0900

    findByUserIdWithProductAndOptions 리팩토링

--- src/main/java/com/todayhouse/domain/order/dao/OrderRepositoryImpl.java
+++ src/main/java/com/todayhouse/domain/order/dao/OrderRepositoryImpl.java
@@ -2,17 +2,14 @@ package com.todayhouse.domain.order.dao;
 
 import com.querydsl.jpa.JPQLQuery;
 import com.todayhouse.domain.order.domain.Orders;
-import com.todayhouse.domain.order.domain.QOrders;
-import com.todayhouse.domain.product.domain.QChildOption;
-import com.todayhouse.domain.product.domain.QParentOption;
-import com.todayhouse.domain.product.domain.QProduct;
-import com.todayhouse.domain.product.domain.QSelectionOption;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 
 import java.util.List;
+
+import static com.todayhouse.domain.order.domain.QOrders.orders;
 
 public class OrderRepositoryImpl extends QuerydslRepositorySupport
         implements CustomOrderRepository {
@@ -22,21 +19,15 @@ public class OrderRepositoryImpl extends QuerydslRepositorySupport
 
     @Override
     public Page<Orders> findByUserIdWithProductAndOptions(Long userId, Pageable pageable) {
-        QOrders qOrder = QOrders.orders;
-        QProduct qProduct = QProduct.product;
-        QChildOption qChildOption = QChildOption.childOption;
-        QParentOption qParentOption = QParentOption.parentOption;
-        QSelectionOption qSelectionOption = QSelectionOption.selectionOption;
+        JPQLQuery<Orders> query = from(orders)
+                .innerJoin(orders.product).fetchJoin()
+                .innerJoin(orders.parentOption).fetchJoin()
+                .leftJoin(orders.childOption).fetchJoin()
+                .leftJoin(orders.selectionOption).fetchJoin()
+                .where(orders.user.id.eq(userId));
 
-        JPQLQuery<Orders> query = from(qOrder)
-                .innerJoin(qOrder.product, qProduct).fetchJoin()
-                .innerJoin(qOrder.parentOption, qParentOption).fetchJoin()
-                .leftJoin(qOrder.childOption, qChildOption).fetchJoin()
-                .leftJoin(qOrder.selectionOption, qSelectionOption).fetchJoin()
-                .where(qOrder.user.id.eq(userId));
-
-        List<Orders> orders = getQuerydsl().applyPagination(pageable, query).fetch();
-        long total = orders.size();
-        return new PageImpl<>(orders, pageable, total);
+        List<Orders> ordersList = getQuerydsl().applyPagination(pageable, query).fetch();
+        long total = ordersList.size();
+        return new PageImpl<>(ordersList, pageable, total);
     }
 }
