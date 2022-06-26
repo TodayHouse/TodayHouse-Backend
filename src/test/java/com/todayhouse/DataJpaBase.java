d2115d59d2e4bc5f24101b47d6bd220dd3a6b921
suker <suker800@gmail.com>
2022-06-26 19:34:16 +0900

    test db mysql로 설정 IntegrationBase.java , DataJpaBase.java - db 테스트시 mysql 불러오게 설정 CategoryControllerTest - delteAllinBatch()설정이 오류나서 deleteAll로 변경 UserServiceImpl - 테스트용 계정 이름 user1이 테스트시 중복되어서 변경

--- src/test/java/com/todayhouse/DataJpaBase.java
+++ src/test/java/com/todayhouse/DataJpaBase.java
@@ -1,9 +1,11 @@
 package com.todayhouse;
 
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 
 @Import(TestConfig.class)
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public abstract class DataJpaBase {
 }
