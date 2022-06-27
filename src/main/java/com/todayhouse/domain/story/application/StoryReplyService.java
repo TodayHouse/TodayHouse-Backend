b38063d0c9b7891a4a2efd3189d6a45525871e3f
suker <suker800@gmail.com>
2022-06-27 14:02:48 +0900

    import 최적화

--- src/main/java/com/todayhouse/domain/story/application/StoryReplyService.java
+++ src/main/java/com/todayhouse/domain/story/application/StoryReplyService.java
@@ -1,8 +1,8 @@
 package com.todayhouse.domain.story.application;
 
-import com.todayhouse.domain.story.dto.reqeust.DeleteReplyRequest;
-import com.todayhouse.domain.story.dto.reqeust.CreateReplyRequest;
-import com.todayhouse.domain.story.dto.response.CreateReplyResponse;
+import com.todayhouse.domain.story.dto.reqeust.ReplyCreateRequest;
+import com.todayhouse.domain.story.dto.reqeust.ReplyDeleteRequest;
+import com.todayhouse.domain.story.dto.response.ReplyCreateResponse;
 import com.todayhouse.domain.story.dto.response.ReplyGetResponse;
 import com.todayhouse.domain.user.domain.User;
 import org.springframework.data.domain.Page;
@@ -10,9 +10,9 @@ import org.springframework.data.domain.Pageable;
 
 
 public interface StoryReplyService {
-    CreateReplyResponse replyStory(User user, CreateReplyRequest request);
+    ReplyCreateResponse replyStory(User user, ReplyCreateRequest request);
 
-    void deleteReply(User user, DeleteReplyRequest request);
+    void deleteReply(User user, ReplyDeleteRequest request);
 
     Page<ReplyGetResponse> findReplies(Long storyId, Pageable pageable);
 }
--- src/main/java/com/todayhouse/domain/story/dao/StoryReplyRepository.java
+++ src/main/java/com/todayhouse/domain/story/dao/StoryReplyRepository.java
@@ -7,8 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.util.UUID;
-
 public interface StoryReplyRepository extends JpaRepository<StoryReply, Long> {
     @Query(value = "select r from StoryReply r " +
             " join fetch r.story s" +
