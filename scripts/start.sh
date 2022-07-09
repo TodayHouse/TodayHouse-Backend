83fe1d9d36025482303532c75d79ca7b102b72fa
Choi Tae gyu <39821474+suker80@users.noreply.github.com>
2022-07-09 23:31:52 +0900

    cicd 파일 스크립트 수정  (#103)
    * cicd 스크립트 경로 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
-BUILD_JAR=./todayhouse-0.0.1-SNAPSHOT.jar
+BUILD_JAR= /home/ec2-user/build/todayhouse-0.0.1-SNAPSHOT.jar
+
 JAR_NAME=$(basename $BUILD_JAR)
 echo "> build : $JAR_NAME" >> /home/ec2-user/deploy.log
 
-echo "> build 파일 복사" >> /home/ec2-user/deploy.log
+echo "> build file copy" >> /home/ec2-user/deploy.log
 DEPLOY_PATH=/home/ec2-user/
 cp $BUILD_JAR $DEPLOY_PATH
 
-echo "> 실행중인 애플리케이션 pid 확인" >> /home/ec2-user/deploy.log
+echo "> check current running application sever" >> /home/ec2-user/deploy.log
 CURRENT_PID=$(pgrep -f $JAR_NAME)
 
 if [ -z $CURRENT_PID ]
 then
-  echo "> 실행중인 애플리케이션이 없으므로 종료하지 않음" >> /home/ec2-user/deploy.log
+  echo "> application didn't started yet" >> /home/ec2-user/deploy.log
 else
   echo "> kill -15 $CURRENT_PID" >> /home/ec2-user/deploy.log
   kill -15 $CURRENT_PID
@@ -20,5 +21,5 @@ else
 fi
 
 DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
-echo "> DEPLOY_JAR 배포"    >> /home/ec2-user/deploy.log
+echo "> DEPLOY_JAR deploy"    >> /home/ec2-user/deploy.log
 nohup java -jar $DEPLOY_JAR >> /home/ec2-user/deploy.log 2>/home/ec2-user/deploy_err.log &
